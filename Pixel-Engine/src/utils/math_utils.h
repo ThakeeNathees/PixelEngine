821bd91af1def597c79741d730af434ff1db416f
ThakeeNathees <thakeenathees@gmail.com>
2019-08-16 00:06:08 +0530

    math utils added more

--- Pixel-Engine/src/utils/math_utils.h
+++ Pixel-Engine/src/utils/math_utils.h
@@ -7,15 +7,17 @@
 namespace pe
 {
 	template<typename T=glm::fvec2>
-	inline glm::fvec2 rotatePoint( float x, float y, float angle_degree ) {
+	T rotatePoint( float x, float y, float angle_degree ) {
 		float angle = angle_degree * PI / 180.0;
-		glm::fvec2 rotated;
+		T rotated;
 		rotated.x = x * glm::cos(angle) - y * glm::sin( angle );
 		rotated.y = y * glm::cos(angle) + x * glm::sin(angle);
 		return rotated;
 	}
-	inline glm::fvec2 rotatePoint(glm::fvec2 point, float angle_degree) { return rotatePoint(point.x, point.y, angle_degree); }
-	inline glm::fvec2 rotatePoint(sf::Vector2f point, float angle_degree) { return rotatePoint(point.x, point.y, angle_degree); }
+	template<typename T = glm::fvec2>
+	T rotatePoint(glm::fvec2 point, float angle_degree) { return rotatePoint(point.x, point.y, angle_degree); }
+	template<typename T = glm::fvec2>
+	T rotatePoint(sf::Vector2f point, float angle_degree) { return rotatePoint(point.x, point.y, angle_degree); }
 
 	template <typename V1, typename V2>
 	V2 convertVector(V1 vec) {
